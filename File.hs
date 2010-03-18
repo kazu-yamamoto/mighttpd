08af0cddd18e885bba25faca689bdc1855c7f7fb
Kazu Yamamoto <kazu@iij.ad.jp>
2010-03-18 15:15:13 +0900

    version 0.3.0

--- File.hs
+++ File.hs
@@ -21,7 +21,7 @@ progName :: String
 progName = "Mighttpd"
 
 progVersion :: String
-progVersion = "0.2.0"
+progVersion = "0.3.0"
 
 progNameVersion :: String
 progNameVersion = progName ++ " " ++ progVersion
